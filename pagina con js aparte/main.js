c76a95f8cda71f4e498a5b2e63896113ee03ec95
FabianCaroR <53945537+FabianCaroR@users.noreply.github.com>
2020-03-05 23:30:08 -0500

    Add files via upload

---
+++ pagina con js aparte/main.js
@@ -0,0 +1,11 @@
+function suma(Numero1, Numero2){
+
+    var Numero1 = parseFloat(document.getElementById("number1").value)
+    var Numero2 = parseFloat(document.getElementById("number2").value)
+    var suma = 0
+
+
+    suma  = Numero1+Numero2
+console.log(Numero1)
+    alert("EL resultado de la suma es "+suma)
+}
